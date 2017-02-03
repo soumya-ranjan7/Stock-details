23de48f00d9e9fcd0a86452b9a2926a9944b46f7
soumya-ranjan7 <soumya.cr17@gmail.com>
2017-02-03 16:18:18 +0530

    Add files via upload

--- stock.py
+++ stock.py
@@ -4,7 +4,7 @@ from flask import render_template
 from bs4 import BeautifulSoup
 import requests
 import json
-
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
@@ -27,25 +27,26 @@ def my_form_post():
 
     data = json.loads(p.string)
 
-    info=data['data']
-    info1 = info[0]
+    info = data['data']
+   
 
-        
-    Last_price = info1["lastPrice"]    
-    PreviousDay_Closing_price = info1["previousClose"]
-    Opening_price = info1["open"]
-    Highest_of_day = info1["dayHigh"]
-    Lowest_of_day = info1["dayLow"]
-    Closing_price = info1["closePrice"]
-    Percentage_change = info1["pChange"]
+    if not info:
+        return render_template("error.html")
+    else:
+        info1 = info[0]
+        Last_price = info1["lastPrice"]    
+        PreviousDay_Closing_price = info1["previousClose"]
+        Opening_price = info1["open"]
+        Highest_of_day = info1["dayHigh"]
+        Lowest_of_day = info1["dayLow"]
+        Closing_price = info1["closePrice"]
+        Percentage_change = info1["pChange"]
     
-    stock_details={"Last price ":Last_price, "PreviousDay Closing price":PreviousDay_Closing_price , "Opening price":Opening_price  , "Highest of day":Highest_of_day
+        stock_details={"Last price ":Last_price, "PreviousDay Closing price":PreviousDay_Closing_price , "Opening price":Opening_price  , "Highest of day":Highest_of_day
                    , "Lowest of day ":Lowest_of_day, "Closing price":Closing_price , "Percentage change":Percentage_change}
        
-    return render_template ("StockInfo.html",details=stock_details)
+        return render_template ("StockInfo.html",details=stock_details)
 
-
-
-
+    
 if __name__ == '__main__':
     app.run(debug=True)
